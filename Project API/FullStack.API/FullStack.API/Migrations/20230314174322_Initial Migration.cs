457176caa8a6f5be266f81c0cc654ca2e0c0d970
Daniel Chaves <85415599+lilkirito22@users.noreply.github.com>
2023-03-14 22:34:50 -0300

    WebApi

---
+++ Project API/FullStack.API/FullStack.API/Migrations/20230314174322_Initial Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FullStack.API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phone = table.Column<long>(type: "bigint", nullable: false),
+                    salary = table.Column<long>(type: "bigint", nullable: false),
+                    departament = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Project API/FullStack.API/FullStack.API/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace FullStack.API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public long phone { get; set; }
+        public long salary { get; set; }
+        public string departament { get; set; }
+
+
+    }
+}
